1b8d548a1cbaeb91a86de84d8816d8c11e32ca83
s-xq <971070045@qq.com>
2019-11-14 20:53:07 +0800

    init repos

---
+++ src/main/java/com/sxq/javaio/JavaioApplication.java
@@ -0,0 +1,13 @@
+package com.sxq.javaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxq/javaio/JavaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxq.javaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
